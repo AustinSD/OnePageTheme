94b4bae53340a161a1a42e83ee3225f383a06e2c
AustinSD <austin.johnson.sd@gmail.com>
2015-05-15 16:20:53 -0700

    button fix and some code clean up

--- server/server/startup.js
+++ server/server/startup.js
@@ -3,4 +3,10 @@ Meteor.startup(function() {
 	if (Meteor.roles.find({name: 'admin'}).count() < 1 ) {
 		Roles.createRole('admin');
 	}
+	if (Meteor.roles.find({name: 'Porter'}).count() < 1 ) {
+		Roles.createRole('Porter');
+	}
+	if (Meteor.roles.find({name: 'Advisor'}).count() < 1 ) {
+		Roles.createRole('Advisor');
+	}
 });
