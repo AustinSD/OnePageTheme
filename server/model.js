50f5f8df0901e470a4ed9474ed507e38fce29441
AustinSD <austin.johnson.sd@gmail.com>
2015-07-11 00:43:07 -0700

    Setting up notes branch

--- server/model.js
+++ server/model.js
@@ -3,6 +3,9 @@
         // bootstrap the admin user if they exist -- You'll be replacing the id later
         if (Meteor.users.findOne("qqhR5d68stCLW92LK"))
             Roles.addUsersToRoles("qqhR5d68stCLW92LK", ['admin']);
+            
+        if (Meteor.users.findOne("nCAZCfDDXXF7iYMdt"))
+            Roles.addUsersToRoles("nCAZCfDDXXF7iYMdt", ['admin']);
 
     });
 Meteor.methods({
